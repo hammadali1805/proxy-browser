e51b17ef89c250b5588e894c2fc98a9180f78efd
Hammad <hammadalipbt18@gmail.com>
2026-04-01 17:05:50 +0530

    refactor: implement service worker fetch handling for improved routing

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,11 @@
 importScripts("/uv/uv.bundle.js");
 importScripts("/uv.config.js");
-importScripts("/uv/uv.handler.js");
+importScripts("/uv/uv.sw.js");
+
+const sw = new UVServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  if (sw.route(event)) {
+    event.respondWith(sw.fetch(event));
+  }
+});
